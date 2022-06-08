a0d29d38b65e7854856e8078f6469e14234e4669
voytula <kacperczyk@outlook.com>
2022-06-08 10:43:05 +0100

    delete option

--- script.js
+++ script.js
@@ -3,12 +3,35 @@ const BASE_URL = 'https://swapi.dev/api/';
 import { Super, People, Planets, Films, Species, Vehicles, Starships } from './classes.js';
 
 let currentData = {};
+let currentCategory = null;
 
+const nextPageButton = document.getElementById('next-page');
+
+nextPageButton.addEventListener('click', async function () {
+	if (currentData.next) {
+		await fetchAPI(currentCategory, currentData.next);
+		loadDataTable(currentCategory);
+	}
+});
+
+function getId(url) {
+	const data = url.split('/');
+	// console.log('splitted', data);
+	return data[data.length - 2];
+}
 //get SWAPI data
-async function fetchAPI(endpoint) {
+async function fetchAPI(category, url) {
 	try {
-		const response = await fetch(`${BASE_URL}${endpoint}`);
+		const fetchUrl = url ? url : `${BASE_URL}${category}`;
+
+		const response = await fetch(fetchUrl);
 		currentData = await response.json();
+		currentData.results = currentData.results.map((item) => {
+			return {
+				...item,
+				id: getId(item.url),
+			};
+		});
 		// console.log(currentData.results);
 	} catch (error) {
 		console.log(`O Matko Bosko, co to sie stanelo?`, error);
@@ -19,30 +42,32 @@ async function fetchAPI(endpoint) {
 async function createButtons() {
 	const response = await fetch(BASE_URL);
 	const data = await response.json();
+	// console.log('data', data);
 	const buttons = document.getElementById('buttons');
-	Object.entries(data).map(([key, _]) => {
-		// console.log(key, value);
+	Object.entries(data).map((entry) => {
+		const key = entry[0];
 		const button = document.createElement('button');
 		button.classList.add('menu-button');
 		button.innerHTML = key.charAt(0).toUpperCase() + key.slice(1);
+		button.dataset.id = key;
 		buttons.appendChild(button);
 		button.addEventListener('click', buttonClick);
 	});
 }
 
+function nextPage() {}
+
 //get correct endpoint data when button clicked
 async function buttonClick(event) {
-	const category = event.target.innerHTML.toLowerCase();
+	console.log(event);
+	const category = event.target.dataset.id;
 	await fetchAPI(category);
-
+	currentCategory = category;
 	// console.log(currentData.results);
 	let resultCount = document.getElementById('number-results');
 	resultCount.innerHTML = `${currentData.count} results`;
-	deleteResultInTable();
 	loadDataTable(category);
 }
-
-function deleteResultInTable() {}
 
 //play some decent music
 function playMusic() {
@@ -59,6 +84,19 @@ function playMusic() {
 		}
 	});
 }
+// REFACTOR
+// const collections = {
+// 	// id, actions
+//   people: ["name", "height", "mass", "gender", "created"],
+// };
+
+// function renderCollection(category) {
+// 	collections[category]
+//     let categoryObj = currentData.results.map(
+//       ({ name, url, created, height, mass, gender }) =>
+//         new People(name, url, created, height, mass, gender)
+//     );
+// }
 
 // load endpoint data on table
 function loadDataTable(category) {
@@ -83,14 +121,15 @@ function loadDataTable(category) {
 		thead.innerHTML = showLabels;
 		categoryObj.forEach((element, index) => {
 			showData += `
-	      <tr>
-	        <td>${index + 1}</td>
+	      <tr id="${element.url}">
+	        <td>${getId(element.url)}</td>
 	        <td>${element.name}</td>
 	        <td>${element.height}</td>
 	        <td>${element.mass}</td>
 	        <td>${element.gender}</td>
 	        <td>${element.created.slice(0, 10)}</td>
-	        <td><img id="info-button" src="./images/info.png"/><img id="delete-button" src="./images/trash.png"/></td>
+	        <td><img id="info-button" src="./images/info.png"/><img class="delete-button"
+			data-id="${element.url}" src="./images/trash.png"/></td>
 	      </tr>`;
 		});
 	} else if (category === 'planets') {
@@ -107,7 +146,7 @@ function loadDataTable(category) {
 	        <td>${element.climate}</td>
 	        <td>${element.population}</td>
 	        <td>${element.created.slice(0, 10)}</td>
-	       	<td><img id="info-button" src="./images/info.png"/><img id="delete-button" src="./images/trash.png"/></td>
+	       	<td><img id="info-button" src="./images/info.png"/><img class="delete-button" src="./images/trash.png"/></td>
 	      </tr>`;
 		});
 	} else if (category === 'films') {
@@ -133,7 +172,7 @@ function loadDataTable(category) {
 	        <td>${element.producer}</td>
 	        <td>${element.director}</td>
 	        <td>${element.created.slice(0, 10)}</td>
-	        <td><img id="info-button" src="./images/info.png"/><img id="delete-button" src="./images/trash.png"/></td>
+	        <td><img id="info-button" src="./images/info.png"/><img class="delete-button" src="./images/trash.png"/></td>
 
 	      </tr>`;
 		});
@@ -160,7 +199,7 @@ function loadDataTable(category) {
 	        <td>${element.designation}</td>
 	        <td>${element.average_height}</td>
 	        <td>${element.created.slice(0, 10)}</td>
-	     	<td><img id="info-button" src="./images/info.png"/><img id="delete-button" src="./images/trash.png"/></td>
+	     	<td><img id="info-button" src="./images/info.png"/><img class="delete-button" src="./images/trash.png"/></td>
 	      </tr>`;
 		});
 	} else if (category === 'vehicles') {
@@ -188,7 +227,7 @@ function loadDataTable(category) {
 	        <td>${element.manufacter}</td>
 	        <td>${element.passengers}</td>
 	        <td>${element.created.slice(0, 10)}</td>
-	       	<td><img id="info-button" src="./images/info.png"/><img id="delete-button" src="./images/trash.png"/></td>
+	       	<td><img id="info-button" src="./images/info.png"/><img class="delete-button" src="./images/trash.png"/></td>
 	      </tr>`;
 		});
 	} else if (category === 'starships') {
@@ -217,11 +256,26 @@ function loadDataTable(category) {
 	        <td>${element.consumables}</td>
 	        <td>${element.crew}</td>
 	        <td>${element.created.slice(0, 10)}</td>
-	      	<td><img id="info-button" src="./images/info.png"/><img id="delete-button" src="./images/trash.png"/></td>
+	      	<td><img id="info-button" src="./images/info.png"/><img class="delete-button" src="./images/trash.png"/></td>
 	      </tr>`;
 		});
 	}
 	table.innerHTML = showData;
+
+	const deleteButtons = document.querySelectorAll('.delete-button');
+	deleteButtons.forEach((button) => {
+		button.addEventListener('click', deleteElement);
+	});
+}
+
+function deleteElement(event) {
+	console.log('delete', event.target.dataset.id);
+	const { id } = event.target.dataset;
+	currentData.results = currentData.results.filter((item) => item.url !== id);
+	loadDataTable(currentCategory);
+	console.log(currentData.results);
+	// const elementToDelete = document.getElementById(id);
+	// elementToDelete.remove()
 }
 
 createButtons();
